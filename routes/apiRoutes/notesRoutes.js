91232ac6696f9ce32ed74c5f71c8659f5047b944
unknown <amadocardenas0@gmail.com>
2021-05-31 21:39:59 -0600

    post route for notes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,10 +1,31 @@
 const router = require('express').router
 const { notes }= require('../../db/db');
 
+// return all notes
 router.get('/notes', (req,res) => {
     res.json(notes);
 });
 
-router.post('/notes', (req,res) => {
+// return notes by id
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+      res.json(result);
+  });
+
+//create note
+app.post('/notes', (req, res) => {
+        // adds new note to notes array
+        notesArray.push(note)
     
-});
+        // saves notes array to db.json
+        fs.writeFileSync(
+            path.join(__dirname, '../db/db.json'),
+            JSON.stringify({ notes: notesArray }, null, 2)
+        );
+    });
+
+// router.post('/notes', (req,res) => {
+//     req.body.id = notes.length.toString();
+    
+// });
+
